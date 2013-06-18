98b3cd9726178f9e337be8e58f1ac0a7d94be462
gawcio <gawcio@10.0.1.110>
2013-06-18 21:55:56 +0200

    Sample code checking current ip.
    It is an example of InjectView and Volley request. Request is sent to
    http service and the response is set to TextView - it should be either
    current ip (as seen from outside) or error details.

--- src/pl/com/foqus/volleyexample/MainActivity.java
+++ src/pl/com/foqus/volleyexample/MainActivity.java
@@ -1,16 +1,52 @@
 
 package pl.com.foqus.volleyexample;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import de.greenrobot.inject.Injector;
+import de.greenrobot.inject.annotation.InjectView;
 
 public class MainActivity extends Activity {
+
+    @InjectView(id = R.id.response)
+    TextView mResponseTextView;
+
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Injector.injectInto(this);
+        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://bot.whatismyipaddress.com/";
+        Listener<String> listener = new Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                mResponseTextView.setText(response);
+            }
+        };
+        ErrorListener errorListener = new ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mResponseTextView.setText(error.getLocalizedMessage());
+            }
+        };
+        mResponseTextView.setText("");
+        Request<?> request = new StringRequest(url, listener, errorListener);
+        mRequestQueue.add(request);
     }
 
     @Override
